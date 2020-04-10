fb8a1d461d5b6b624d28138dc5b078d066fdd9b3
tanqingfu <1902952234@qq.com>
2020-04-10 16:35:23 +0800

    测试上传更新

--- src/main/java/com/demo/task/Test_task.java
+++ src/main/java/com/demo/task/Test_task.java
@@ -1,7 +1,10 @@
 package com.demo.task;
 
+import com.demo.util.DateUtils;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import java.util.Date;
 
 @Component
 public class Test_task {
@@ -9,8 +12,12 @@ public class Test_task {
     //*/5 * * * * ?  5秒执行一次
 	@Scheduled(cron="0 */1 * * * ?")
 	public void Test_task(){
-
 		System.out.println("定时1分钟执行一次");
+	}
+
+	@Scheduled(cron = "*/5 * * * * ?")
+	public  void time(){
+		System.out.println("每5秒执行一次："+ DateUtils.dateToString(new Date(),"yyyy-MM-dd HH:mm:ss"));
 	}
 
 }

7f690e2e0bf2df27b50dd42bb073181de29cedb7
tanqingfu <1902952234@qq.com>
2020-04-13 16:55:25 +0800

    202004131533上传

--- src/main/java/com/demo/controller/IndexController.java
+++ src/main/java/com/demo/controller/IndexController.java
@@ -2,6 +2,7 @@ package com.demo.controller;
 
 import com.demo.common.Globals;
 import com.demo.common.InquiryCondition;
+import com.demo.common.JsonData;
 import com.demo.meta.user;
 import com.demo.service.userService;
 import org.springframework.stereotype.Controller;
@@ -12,6 +13,7 @@ import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.List;
 
 @Controller
@@ -35,6 +37,22 @@ public class IndexController extends BaseController{
         request.setAttribute("list",list);
         System.out.println(Globals.WWW_HOST);
         return "index";
+
+    }
+    @RequestMapping(value = "/getData",method = RequestMethod.POST)
+    public void getData(HttpServletRequest request,HttpServletResponse response) throws  IOException{
+        response.setContentType("text/html;charset=UTF-8"); //解决返回数据中文出现乱码
+        InquiryCondition InquiryCondition = new InquiryCondition();
+        int total = userService.countALL(InquiryCondition);
+        setPagePrmt_line(request, InquiryCondition, total, "1", 10);
+        List<user> list = userService.selectAll(InquiryCondition);
+        PrintWriter out = null;
+        out = response.getWriter();
+        JsonData JsonData = new JsonData();
+        if(list.size()>0){
+            JsonData = new JsonData(Globals.SUCCESS,"",list,total);
+        }
+        out.write(JsonData.toJsonString());
 
     }
 }

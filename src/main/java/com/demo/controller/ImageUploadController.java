1273906019a4d7e45e6655184d50e5b08a3a7b9e
tanqingfu <1902952234@qq.com>
2020-04-15 11:20:25 +0800

    增加打印sql语句配置

--- src/main/java/com/demo/controller/ImageUploadController.java
+++ src/main/java/com/demo/controller/ImageUploadController.java
@@ -21,20 +21,20 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.demo.common.Globals;
 import com.demo.util.DateUtils;
-import org.springframework.core.io.FileSystemResource;
+
+import com.sun.image.codec.jpeg.JPEGCodec;
+import com.sun.image.codec.jpeg.JPEGImageEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
 
-import com.sun.image.codec.jpeg.JPEGCodec;
-import com.sun.image.codec.jpeg.JPEGImageEncoder;
+
+
 
 
 
@@ -202,7 +202,7 @@ public class ImageUploadController{
 		return msg;
 	}
 	public  BufferedImage  getNewImage(MultipartFile oldImage,double width,double height) throws IOException{  
-		 /*srcURl 原图地址；deskURL 缩略图地址；comBase 压缩基数；scale 压缩限制(宽/高)比例*/  
+		 /*srcURl 原图地址；deskURL 缩略图地址；comBase 压缩基数；scale 压缩限制(宽/高)比例*/
         ByteArrayInputStream bais = new ByteArrayInputStream(oldImage.getBytes());   
         MemoryCacheImageInputStream mciis = new MemoryCacheImageInputStream(bais);        
         Image src = ImageIO.read(mciis);  
